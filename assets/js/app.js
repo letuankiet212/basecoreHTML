90e7f9a369f42c6b64fe293bb17d7fdbdfbe10f9
letuankiet212 <48205415+letuankiet212@users.noreply.github.com>
2021-12-07 17:02:44 +0700

    up

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,9 @@
 jQuery(document).ready(function () {
-	//Navbar right
-	$(".i-control").click(function () {
-		$(".anchor").toggleClass("open");
-		$(this).toggleClass("i-control-open"); // active bg btn
-	});
+  //Navbar right
+  $('.i-control').click(function () {
+    $('.anchor').toggleClass('open');
+    $(this).toggleClass('i-control-open'); // active bg btn
+  });
 
-	//Img error
-	$("img").on("error", function () {
-		$(this).attr("src", "./assets/images/missing.png");
-	});
+  /* customJS */
 });
